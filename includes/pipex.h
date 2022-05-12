03e9b9cc5824b6a81699f4f56a3e92d9fda0c522
Jee hyun PARK <jeepark@e3r12p11.clusters.42paris.fr>
2022-05-12 15:39:25 +0200

    **pipes done, next create child process depending on nb_builtin

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,13 +6,16 @@
 #include "libft.h"
 
 #define FORK_ERROR 1
-
+#define PIPE_ERROR 2
+#define NB_BUILTIN 3
 typedef struct s_cmd 
 {
     char    *commande;
-    char    **path;
+    char    **path_from_envp;
+    char    *path;
     int     *fd;
-    int     nb_builtin;   
+    int     nb_builtin;
+    int     **pipes;
 }       t_cmd;
 
 #endif
--- test.c
+++ test.c
@@ -9,10 +9,10 @@ void get_path(t_cmd *cmd, char *envp)
      int i = 0;
     (void)cmd;
     
-    cmd->path = ft_split(envp,':');
-    while(cmd->path[i])
+    cmd->path_from_envp = ft_split(envp,':');
+    while(cmd->path_from_envp[i])
     {
-        cmd->path[i] = ft_strjoin(cmd->path[i], "/");
+        cmd->path_from_envp[i] = ft_strjoin(cmd->path_from_envp[i], "/");
         i++;
     }
 
@@ -42,42 +42,41 @@ void	locate_path(t_cmd *cmd, char **envp)
 
 /* Je parcours la liste des chemins pour trouver celui qui correspond a
 ma commande. */
-char	*get_cmd_path(t_cmd *cmd, char *cmd_path)
+int	get_cmd_path(t_cmd *cmd)
 {
 	int i;
 
 	i = 0;
 
-	while (cmd->path[i])
+	while (cmd->path_from_envp[i])
 	{
-		cmd_path = ft_strjoin(cmd->path[i], cmd->commande);
-		if (access(cmd_path, F_OK | X_OK) == 0)
-			return(cmd_path);
+		cmd->path = ft_strjoin(cmd->path_from_envp[i], cmd->commande);
+		if (access(cmd->path, F_OK | X_OK) == 0)
+			return(0);
 		i++;
 	}
-	return (NULL);
+	return (1);
 
 }
 
-void	execute_builtin(char *cmd_path, char **envp)
+void	execute_builtin(t_cmd *cmd, char **envp)
 {
 	char	*options[3] = {"ls", "-la", NULL};
 
-	if (execve(cmd_path, options, envp) == 0)
+	if (execve(cmd->path, options, envp) == 0)
 	 	printf("EXECVE SUCCES !\n");
 }
 
-int	create_pipes(t_cmd *cmd, char *cmd_path, char **envp)
+int	create_pipes(t_cmd *cmd, char **envp)
 {
 	(void)cmd;
-	(void)cmd_path;
 	(void)envp;
 
 	int end[2];
 	pid_t pid;
-	int reader;
-	char buf[10];
-
+	//int reader;
+	//char buf[10];
+	int status;
 
 	if (pipe(end) < 0)
 		return (1);
@@ -89,7 +88,7 @@ int	create_pipes(t_cmd *cmd, char *cmd_path, char **envp)
 	{
 		// je suis dans le processus fils
 		close(end[0]); // l'enfant ne va rien lire alors je ferme le bout lecture
-		write(end[1], "coucou\n", 7);
+		execute_builtin(cmd, envp);
 		close(end[1]); // l'enfant a fini d'ecrire alors je ferme le bout ecriture 
 		return (0);
 	}
@@ -97,36 +96,84 @@ int	create_pipes(t_cmd *cmd, char *cmd_path, char **envp)
 	{
 		// je suis dans le processus parent
 		close(end[1]); // le parent ne va rien ecrire alors je ferme le bout lecture
-		waitpid(pid, NULL, 0); // le parent attend son fils
-		reader = read(end[0], buf, 10);
-		if (reader < 0)
-			return (3);
-		close(end[0]); // le parent a fini de lire alors je ferme le bout lecture
-		buf[reader] = '\0';
-		printf("%s\n", buf);
+		waitpid(pid, &status, 0); // le parent attend son fils
+		if (WIFEXITED(status))
+            printf("Pere : Il a termine normalement, code sortie %d\n", WEXITSTATUS(status));
+        else
+            printf("Pere : Il a ete interrompu...\n");
 	}
 	return (0);
+}
+
+int **destroy_pipes(int **pipes)
+{
+	int	i;
+
+	i = 0;
+	while (pipes[i])
+	{
+		free(pipes[i]);
+		i++;
+	}
+	free(pipes);
+	return (NULL);
+}
+
+int	init_pipes(t_cmd *cmd)
+{
+	int i;
+
+	cmd->pipes = malloc(sizeof(int *) * (NB_BUILTIN - 1));
+	if (!cmd->pipes)
+		return (PIPE_ERROR);
+	i = 0;
+	while(i < NB_BUILTIN - 1)
+	{
+		cmd->pipes[i] = malloc(sizeof(int) * 2);
+		if (!cmd->pipes[i])
+			return(destroy_pipes(cmd->pipes), PIPE_ERROR);
+		if (pipe(cmd->pipes[i]) == -1)
+			return(PIPE_ERROR);
+		cmd->pipes[i][0] = 0;
+		cmd->pipes[i][1] = 1;
+		i++;
+	}
+	return(0);
 }
 
 
 int main(int ac, char **av, char **envp)
 {
     t_cmd	cmd;
-	char	*cmd_path;
-
+	int		**pipes;
 	cmd.nb_builtin = 3;
 	
 	if (ac > 1)
 	{
 		cmd.commande = av[1];
 	}
-	cmd_path = NULL;
+	pipes = NULL;
+	cmd.path = NULL;
 	locate_path(&cmd, envp);
-	cmd_path = get_cmd_path(&cmd, cmd_path);
-	printf("PATH FOR LS IS : %s\n", cmd_path);
-	printf("%d\n", create_pipes(&cmd, cmd_path, envp));
+	get_cmd_path(&cmd);
+	printf("PATH FOR %s IS : %s\n", av[1], cmd.path);
+	if (init_pipes(&cmd) == PIPE_ERROR)
+		return (write(2, "pipe error\n", 11), 0);
+	//printf("%d\n", create_pipes(&cmd, envp));
 	// if (cmd.nb_builtin == 1)
 	// 	execute_builtin(cmd_path, envp);
-
+	int i = 0;
+	int j = 0;
+	while( i < NB_BUILTIN - 1)
+	{
+		j = 0;
+		while(j < 2)
+		{
+			printf("PIPE no. %d = %d ", i, cmd.pipes[i][j]);
+			j++;
+		}
+		i++;
+		printf("\n");
+	}
     return (0);
 }
